8e0e5d6b8b24e17fd3460c9e4373550ef2dfef59
Aaditya Tile <adi01tile@gmail.com>
2023-06-29 00:33:23 +0530

    create diary inprogress

--- static/js/script.js
+++ static/js/script.js
@@ -8,3 +8,12 @@ function addLink() {
 	const url = prompt('Insert url: ');
 	toggleFormatting('createLink', url);
 }
+function save_diary() {
+	var title = document.querySelector('.title-input').value;
+	var tag = document.querySelector('.tag-input').value;
+	var content = document.getElementById('diary-content').innerHTML;
+
+	console.log("Diary Title:", title);
+	console.log("Diary Tag:", tag);
+	console.log("Diary Content:", content);
+}
